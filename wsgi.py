9038d1e2b557a5a091e7b46a3f6e2764ebf90580
Jefferson <jegor.28.a@gmail.com>
2024-05-31 08:00:04 -0500

    wspi actualizado

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proy_sales_may_2024.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proy_sales_may_2024-main.settings')
 
 application = get_wsgi_application()
